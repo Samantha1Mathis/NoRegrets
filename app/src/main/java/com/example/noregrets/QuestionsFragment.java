0322897669f87c89e871b1dcb72c1b7963d464e0
Jacob Hurley <JacobHurley@email.arizona.edu>
2020-12-08 12:12:33 -0700

    fixed last wrong question back button

--- app/src/main/java/com/example/noregrets/QuestionsFragment.java
+++ app/src/main/java/com/example/noregrets/QuestionsFragment.java
@@ -19,6 +19,7 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -88,7 +89,7 @@ public class QuestionsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         this.v = inflater.inflate(R.layout.fragment_questions, container, false);
         v.setFocusableInTouchMode(true);
@@ -98,8 +99,10 @@ public class QuestionsFragment extends Fragment {
             @Override
             public boolean onKey( View v, int keyCode, KeyEvent event )
             {
+                System.out.println("Keycode is: " + keyCode);
                 if( keyCode == KeyEvent.KEYCODE_BACK )
                 {
+
                     return true;
                 }
                 return false;
@@ -204,6 +207,14 @@ public class QuestionsFragment extends Fragment {
                             }
                             animRotate = AnimationUtils.loadAnimation(containerActivity,
                                     R.anim.rotate);
+                            containerActivity.getWindow().getDecorView().setSystemUiVisibility(
+                                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                                            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
 
                             new FreezeApp(animRotate,iv).execute("");
                             ((MainActivity) getActivity()).NumberAnswered = 0;
@@ -469,6 +480,10 @@ public class QuestionsFragment extends Fragment {
          * @param json
          */
         protected void onPostExecute(JSONObject json){
+            containerActivity.getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
             ((MainActivity)getActivity()).nextClick(v);
         }
 
