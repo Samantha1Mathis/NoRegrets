a8a2b7481d9a3ac64dcc7408f219b565ba6fa47e
Jacob Hurley <JacobHurley@email.arizona.edu>
2020-12-08 19:41:30 -0700

    attempted to fix navigation bar

--- app/src/main/java/com/example/noregrets/QuestionsFragment.java
+++ app/src/main/java/com/example/noregrets/QuestionsFragment.java
@@ -214,9 +214,9 @@ public class QuestionsFragment extends Fragment {
                             containerActivity.getWindow().getDecorView().setSystemUiVisibility(
                                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                            //| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                                            //| View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                                             | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
 
@@ -484,8 +484,8 @@ public class QuestionsFragment extends Fragment {
         protected void onPostExecute(JSONObject json){
             containerActivity.getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+                            //| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
             ((MainActivity)getActivity()).nextClick(v);
         }
 
